41fc6a543cc947197c6552b36e11dba5c5ba53fa
Fernanda <FernandaRojas152@hotmail.com>
2021-02-17 20:01:59 -0500

    adding window

---
+++ colombia-international-treaties/Form1.cs
@@ -0,0 +1,31 @@
+﻿using GMap.NET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+
+namespace colombia_international_treaties
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gmap_Load(object sender, EventArgs e)
+        {
+            gmap.MapProvider = GoogleMapProvider.Instance;  //Proveedor del servicio
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            gmap.Position = new PointLatLng(3.42158, -76.5205);
+        }
+    }
+}
